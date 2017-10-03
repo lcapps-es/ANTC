edb1830fa69ad7157d12d2424d2f7c08b41f40df
amora <adrianmora784@gmail.com>
2017-10-03 11:15:46 +0200

    update clock, styles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	$('.background').addClass('show');
+	$('#background').css('background-image', "url('https://source.unsplash.com/random/1920x1080')").waitForImages(function() {
+		$('#background').addClass('show');
+	}, $.noop, true);
+
+	updateClock();
 });
+
+function updateClock() {
+    var now = new Date(); // current date
+	time = now.getHours() + ':' + ("0" + now.getMinutes()).slice(-2) + ':' + ("0" + now.getSeconds()).slice(-2);
+
+    // set the content of the element with the ID time to the formatted string
+    $('#clock h1').html(time);
+
+    // call this function again in 1000ms
+    setTimeout(updateClock, 1000);
+}
