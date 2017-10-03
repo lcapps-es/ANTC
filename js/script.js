37c3fab8911ab38e3f69b073e03d62ef651b8aa9
Alvaro Tellez <atellez@imaweb.net>
2017-10-03 11:37:26 +0200

    Base Api para el tiempo

--- js/script.js
+++ js/script.js
@@ -16,3 +16,46 @@ function updateClock() {
     // call this function again in 1000ms
     setTimeout(updateClock, 1000);
 }
+
+
+function getLocation(callback) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+			console.log(position);
+			callback(position.coords);
+		});
+	} else {
+		console.info( "Geolocation is not supported by this browser.");
+	}
+}
+
+
+function getWeather() {
+	var path = "http://samples.openweathermap.org/data/2.5/weather?appid=b1b15e88fa797225412429c1c50c122a1" //lat=35&lon=139&
+
+	getLocation(function(pos){		
+			var url = path + "&lat=" + pos.latitude + "&lon=" + pos.longitude;
+			console.log(url);
+	});
+	
+
+	/*
+	var xhttp = new XMLHttpRequest();
+	var url = getUrl();
+
+	xhttp.open("GET", getUrl(), true);
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			navigator.splashscreen.hide();
+			offline(false);
+			document.location = getUrl();
+		} else if (this.readyState == 4) {
+			offline();
+		}
+	};
+	xhttp.send();
+
+	*/
+
+
+}
